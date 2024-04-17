469f2bf5ef5c1a8dcd6577e5de1e7c4391288546
Suresh <sureshb9a11460@gmail.com>
2024-04-17 17:55:48 +0530

    intial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Box -1
+const box_1 = document.getElementById("box-one");
+const inputname = document.getElementById("name");
+const inputemail = document.getElementById("email");
+const btn_1 = document.getElementById("first-btn");
+
+// Box-2
+const box_2 = document.getElementById("box-two");
+const topic_1 = document.getElementById("topic-1");
+const topic_2 = document.getElementById("topic-2");
+const topic_3 = document.getElementById("topic-3");
+const btn_2 = document.getElementById("second-btn");
+
+// Box-3
+const box_3 = document.getElementById("box-three");
